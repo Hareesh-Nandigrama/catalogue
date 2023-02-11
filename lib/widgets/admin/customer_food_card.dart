4d9f7ee2f0023328ef69e2a4d4a75c9c828c7d1d
Ashutosh-777 <ashutosh.ghosty@gmail.com>
2023-02-12 02:48:04 +0530

    customer card1

--- lib/widgets/admin/customer_food_card.dart
+++ lib/widgets/admin/customer_food_card.dart
@@ -5,82 +5,87 @@ class Customercard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Container(
-              clipBehavior: Clip.antiAlias,
-              decoration:  BoxDecoration(
-                borderRadius: BorderRadius.circular(15),
+        body: Container(
+          margin: const EdgeInsets.all(16),
+            clipBehavior: Clip.antiAlias,
+            decoration:  BoxDecoration(
+              borderRadius: BorderRadius.circular(15),
+              color: Colors.white,
+                boxShadow: const [
+                BoxShadow(
+                  color: Color.fromRGBO(0, 0, 0, 0.25),
+                  blurRadius: 10,
+                  spreadRadius: 1,
+                  offset: Offset(0, 10),
+                )]
+            ),
+            height: 250,
+            width: double.infinity,
+            child: Column(
+              children: [
+                Stack(
+                    alignment: Alignment.bottomLeft,
+                    children: [
+                      ClipRRect(
+                        child: Image.asset(
+                          'assets/beach.jpeg',
+                          fit: BoxFit.cover,
+                          height: 192,
+                          width: double.infinity,
+                        ),
 
-              ),
-              height: 250,
-              width: double.infinity,
-              child: Column(
-                children: [
-                  Stack(
-                      alignment: Alignment.bottomLeft,
-                      children: [
-                        ClipRRect(
-                          child: Image.asset(
-                            'assets/beach.jpeg',
-                            fit: BoxFit.cover,
-                            height: 192,
-                            width: double.infinity,
-                          ),
-
-                        ),
-                        const Padding(
-                          padding: EdgeInsets.only(left: 17,bottom: 12),
-                          child: Text('Roasted Pot',style: TextStyle(
-                              fontSize: 24,
-                              color: Colors.white
-                          ),),
-                        ),
-                        const Padding(
-                          padding: EdgeInsets.only(left: 17,top: 12),
-                          child: Text('Description of Roasted Pot',style: TextStyle(
-                              fontSize: 12,
-                              color: Colors.white
-                          ),),
-                        )
-                      ]
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 17,top: 8),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: const [
-                        Icon(Icons.circle,
-                          color: Colors.green,
-                          size: 8,
-                        ),
-                        Text('  Currently open',style: TextStyle(
-                          fontSize: 10,
+                      ),
+                      const Padding(
+                        padding: EdgeInsets.only(left: 17,bottom: 12),
+                        child: Text('Roasted Pot',style: TextStyle(
+                            fontSize: 24,
+                            color: Colors.white
                         ),),
-                      ],
-                    ),
+                      ),
+                      const Padding(
+                        padding: EdgeInsets.only(left: 17,top: 12),
+                        child: Text('Description of Roasted Pot',style: TextStyle(
+                            fontSize: 12,
+                            color: Colors.white
+                        ),),
+                      )
+                    ]
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 17,top: 8),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: const [
+                      Icon(Icons.circle,
+                        color: Colors.green,
+                        size: 8,
+                      ),
+                      Text('  Currently open',style: TextStyle(
+                        fontSize: 10,
+                      ),),
+                    ],
                   ),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 17,top: 2),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: const [
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 17,top: 2),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: const [
 
-                        Icon(Icons.circle,
-                          color: Colors.green,
-                          size: 8,
+                      Icon(Icons.circle,
+                        color: Colors.green,
+                        size: 8,
+                      ),
+                      Text('  Accepting orders',
+                        style: TextStyle(
+                            fontSize: 10
                         ),
-                        Text('  Accepting orders',
-                          style: TextStyle(
-                              fontSize: 10
-                          ),
-                        ),
-                      ],
-                    ),
-                  )
-                ],
-              )
-          ),
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            )
         ),
       );
   }

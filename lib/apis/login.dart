014b46e619426e07d9356f0534f9bf297482da9e
Priyanshu Srivastava <priyanshusrivastava42@gmail.com>
2023-02-16 11:24:07 +0530

    create shopkeeper endpoint

--- lib/apis/login.dart
+++ lib/apis/login.dart
@@ -1,9 +1,42 @@
+import 'dart:convert';
+
 import 'package:http/http.dart' as http;
 
-Future<void> getShopkeeper() async {
+const baseUrl = 'https://odd-plum-clam-cuff.cyclic.app/';
+
+Future<void> createShopkeeper(
+    String phoneNumber,
+    String email,
+    String name,
+    String businessName,
+    String businessType,
+    String location,
+    int closeTime,
+    int openTime,
+    List<String> photos,
+    String description,
+    String uid) async {
+  final body = jsonEncode({
+    "phoneNumber": phoneNumber,
+    "email": email,
+    "name": name,
+    "businessName": businessName,
+    "businessType": businessType,
+    "location": location,
+    "openTime": openTime,
+    "closeTime": closeTime,
+    "photos": photos,
+    "description": description,
+    "uid": uid
+  });
+
   final response = await http.post(
-      Uri.parse('https://odd-plum-clam-cuff.cyclic.app/api/shopkeeper'),
-      headers: {"Authorization": "Token qwerty"});
+    Uri.parse('${baseUrl}api/shopkeeper'),
+    body: body,
+    headers: {
+      'content-type': 'application/json'
+    }
+  );
 
   print(response.body);
 }

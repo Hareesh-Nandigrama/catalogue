62acf1e70cdd60ed8d83dd6d69a85e2d06167780
Ashutosh-777 <ashutosh.ghosty@gmail.com>
2023-02-17 22:32:06 +0530

    customer card

--- lib/widgets/customer/customer_card.dart
+++ lib/widgets/customer/customer_card.dart
@@ -1,87 +1,81 @@
 import 'package:flutter/material.dart';
-
-import '../../screens/customer/shop_details_page.dart';
 class CustomerCard extends StatelessWidget {
   final body;
   const CustomerCard({Key? key, this.body}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: (){
-        if(body['businessType'] == 'foodOutlet')
-          {
-            Navigator.of(context).push(MaterialPageRoute(builder: (context) => ShopDetailsPage(
-
-            )));
-
-          }
-      },
+    return Center(
       child: Container(
-            margin: const EdgeInsets.symmetric(horizontal: 16),
-              clipBehavior: Clip.antiAlias,
-              decoration:  BoxDecoration(
-                borderRadius: BorderRadius.circular(15),
-                color: Colors.white,
-                  boxShadow: const [
-                  BoxShadow(
-                    color: Color.fromRGBO(0, 0, 0, 0.25),
-                    blurRadius: 10,
-                    spreadRadius: 1,
-                    offset: Offset(0, 10),
-                  )]
-              ),
-              height: 250,
-              width: double.infinity,
-              child: Column(
-                children: [
-                  Stack(
-                      alignment: Alignment.bottomLeft,
-                      children: [
-                        ClipRRect(
-                          child: Image.asset(
-                            'assets/beach.jpeg',
-                            fit: BoxFit.cover,
-                            height: 192,
-                            width: double.infinity,
-                          ),
+          margin: const EdgeInsets.symmetric(horizontal: 16),
+          clipBehavior: Clip.antiAlias,
+          decoration:  BoxDecoration(
+              borderRadius: BorderRadius.circular(15),
+              color: Colors.white,
+              boxShadow: const [
+                BoxShadow(
+                  color: Color.fromRGBO(0, 0, 0, 0.25),
+                  blurRadius: 10,
+                  spreadRadius: 1,
+                  offset: Offset(0, 10),
+                )]
+          ),
+          height: 242,
+          width: double.infinity,
+          child: Column(
+            children: [
+              Stack(
+                  alignment: Alignment.bottomLeft,
+                  children: [
+                    ClipRRect(
+                      child: Image.network(
+                        'https://th.bing.com/th/id/OIP.Wp6XgnYRrIXB2P0mSTBhBAHaEo?pid=ImgDet&rs=1',
+                        fit: BoxFit.cover,
+                        height: 192,
+                        width: double.infinity,
+                      ),
 
-                        ),
-                        Padding(
-                          padding: EdgeInsets.only(left: 17,bottom: 12),
-                          child: Text(body['businessName'],style: TextStyle(
-                              fontSize: 24,
-                              color: Colors.white
-                          ),),
-                        ),
-                        Padding(
-                          padding: EdgeInsets.only(left: 17,top: 12),
-                          child: Text(body['businessName'],style: TextStyle(
-                              fontSize: 12,
-                              color: Colors.white
-                          ),),
-                        )
-                      ]
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 17,top: 8),
-                    child: Row(
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 17,bottom: 14),
+                      child: Text(body['businessName'],style: const TextStyle(
+                          fontSize: 24,
+                          color: Colors.white
+                      ),),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 17,top: 12),
+                      child: Text(body['description'],style: const TextStyle(
+                          fontSize: 12,
+                          color: Colors.white
+                      ),),
+                    )
+                  ]
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 12,vertical: 6),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
                       mainAxisAlignment: MainAxisAlignment.start,
-                      children: const [
-                        Icon(Icons.circle,
-                          color: Colors.green,
-                          size: 8,
+                      children:  [
+                        const Icon(Icons.location_on,
+                          size: 18,
                         ),
-                        Text('  Currently open',style: TextStyle(
-                          fontSize: 10,
+                        Text('    ${body['location']}',style: const TextStyle(
+                          fontSize: 16,
                         ),),
                       ],
                     ),
-                  ),
+                    const Icon(Icons.call,color: Colors.green,),
+                  ],
+                ),
+              ),
 
-                ],
-              )
-          ),
+            ],
+          )
+      ),
     );
   }
 }

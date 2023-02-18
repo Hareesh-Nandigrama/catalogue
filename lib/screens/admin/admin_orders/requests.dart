cd47cc76b24df5d6ab5d95b7f0035db015b54229
Ashutosh-777 <ashutosh.ghosty@gmail.com>
2023-02-18 20:34:44 +0530

    hehe

--- lib/screens/admin/admin_orders/requests.dart
+++ lib/screens/admin/admin_orders/requests.dart
@@ -1,14 +1,24 @@
 import 'package:catalogue/widgets/admin/Order_accept_deny_page.dart';
+import 'package:catalogue/widgets/common/custom_progress.dart';
 import 'package:flutter/material.dart';
-
+import '../../../apis/orders.dart';
 class Requests extends StatelessWidget {
   const Requests({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemBuilder: (context, index) => OrderCardAcceptorDeny(),
-      itemCount: 10,
+    return FutureBuilder(
+      builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+        future: getPendingOrders();
+        if(snapshot.hasData){
+          print(snapshot.data);
+          return ListView.builder(
+            itemBuilder: (context, index) => const OrderCardAcceptorDeny(),
+            itemCount: 10,
+          );
+        }else{
+          return const CustomProgress();
+        }
+      },
     );
   }
 }
